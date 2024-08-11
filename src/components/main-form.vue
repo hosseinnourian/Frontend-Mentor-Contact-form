cbceed90e33b56d1be045c25ba1affab42df6fe0
hosseinnourian <hosseinnourian@protonmail.com>
2024-08-11 12:58:59 +0330

    add basic layout

---
+++ src/components/main-form.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="container">
+        <h1>Main Form</h1>
+    </div>
+</template>
+
+
+
+<style scoped>
+.container{
+    width: 60%;
+    margin: 50px auto;
+    background-color: rgb(226, 230, 233);
+    border-radius: 18px;
+}
+h1 {
+    text-align: center;
+}
+</style>
